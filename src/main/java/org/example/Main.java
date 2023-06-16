7f2f512deb63c7fb1eac9ecfb9122d5603806de3
mperusso <matheus-perusso@hotmail.com>
2023-06-15 23:03:31 -0300

    feat: PlusMinus problem

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,27 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        //PLUS MINUS TEST
+        List<Integer> array = new ArrayList<>();
+        array.add(-4);
+        array.add(3);
+        array.add(-9);
+        array.add(0);
+        array.add(4);
+        array.add(1);
+
+        PlusMinus.plusMinus(array);
     }
 }
--- src/main/java/org/example/PlusMinus.java
+++ src/main/java/org/example/PlusMinus.java
@@ -1,2 +1,39 @@
-package org.example;public class PlusMinus {
+package org.example;
+
+import java.text.DecimalFormat;
+import java.util.List;
+
+public class PlusMinus {
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<Integer> arr) {
+        // Write your code here
+
+        int positives = 0;
+        int negatives = 0;
+        int zeros = 0;
+        int size = arr.size();
+
+        DecimalFormat format = new DecimalFormat("0.000000");
+
+        for(int number : arr) {
+            if (number > 0) {
+                positives++;
+            } else if (number < 0) {
+                negatives++;
+            } else {
+                zeros++;
+            }
+        }
+
+        System.out.println(format.format((double)positives/size));
+        System.out.println(format.format((double)negatives/size));
+        System.out.println(format.format((double)zeros/size));
+
+    }
 }
